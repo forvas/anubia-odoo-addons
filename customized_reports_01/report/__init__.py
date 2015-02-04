4102610a93e43e839110f62cb04c8ba46f7fc34f
Juan Formoso <jfv@anubia.es>
2015-02-04 11:34:02 +0100

    [IMP] Delivery note lines grouped by product name in reports

--- customized_reports_01/report/__init__.py
+++ customized_reports_01/report/__init__.py
@@ -5,3 +5,4 @@
 ################################################################
 
 from . import account_invoice_report_country
+from . import picking
--- customized_reports_01/report/picking.py
+++ customized_reports_01/report/picking.py
@@ -8,6 +8,7 @@ import time
 from report import report_sxw
 from osv import osv
 import pooler
+from collections import defaultdict
 
 
 class picking(report_sxw.rml_parse):
@@ -16,6 +17,7 @@ class picking(report_sxw.rml_parse):
         self.localcontext.update({
             'time': time,
             'get_qtytotal': self._get_qtytotal,
+            'stock_moves_grouped_by_name': self._stock_moves_grouped_by_name,
         })
 
     def _get_qtytotal(self, move_lines):
@@ -25,6 +27,28 @@ class picking(report_sxw.rml_parse):
             total += move.product_qty
         return {'quantity': total, 'uom': uom}
 
-report_sxw.report_sxw('report.stock.picking.list', 'stock.picking',
+    def _stock_moves_grouped_by_name(self):
+        ids = pooler.get_pool(self.cr.dbname).get('stock.move').search(
+            self.cr, self.uid, [('picking_id', '=',
+                                 self.localcontext['active_id'])],
+            context=self.localcontext)
+        stock_moves = pooler.get_pool(self.cr.dbname).get('stock.move').browse(
+            self.cr, self.uid, ids, context=self.localcontext)
+
+        groups = defaultdict(list)
+        for obj in stock_moves:
+            groups[obj.name].append(obj)
+        grouped_list_by_name = groups.values()
+
+        stock_moves_grouped_by_name = []
+        for same_name_obj_list in grouped_list_by_name:
+            for obj in same_name_obj_list:
+                if obj == same_name_obj_list[0]:
+                    continue
+                same_name_obj_list[0].product_qty += obj.product_qty
+            stock_moves_grouped_by_name.append(same_name_obj_list[0])
+        return stock_moves_grouped_by_name
+
+report_sxw.report_sxw('report.stock.picking.list.custom', 'stock.picking',
                       'addons/customized_reports_01/report/picking.rml',
                       parser=picking)
