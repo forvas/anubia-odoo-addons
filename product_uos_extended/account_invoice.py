95519d7bf3b32b494cd1d7e373e9bf290ff255ea
Juan Formoso <jfv@anubia.es>
2015-03-05 11:03:42 +0100

    [FIX] Rectification invoice error fixed when generating it from Invoices and Rectification Invoices menus

--- product_uos_extended/account_invoice.py
+++ product_uos_extended/account_invoice.py
@@ -26,7 +26,8 @@ class account_invoice_line(osv.osv):
             for l in context.get('invoice_line', {}):
                 if isinstance(l, (list, tuple)) and len(l) >= 3 and l[2]:
                     tax_obj = self.pool.get('account.tax')
-                    p = l[2].get('price_unit', 0) * (1 - l[2].get('discount', 0) / 100.0)
+                    p = l[2].get('price_unit', 0) * (1 - l[2].get(
+                        'discount', 0) / 100.0)
                     t = t - (p * l[2].get('quantity'))
                     taxes = l[2].get('invoice_line_tax_id')
                     if len(taxes[0]) >= 3 and taxes[0][2]:
@@ -41,7 +42,6 @@ class account_invoice_line(osv.osv):
             return t
         return 0
 
-    
     def _get_uos_id(self, cr, uid, *args):
         try:
             proxy = self.pool.get('ir.model.data')
@@ -84,7 +84,7 @@ class account_invoice_line(osv.osv):
             readonly=True, store=False, string='Unit of Sale'),
         'quantity': fields.float(
             'Quantity',
-#             required=True),
+            # required=True),
             required=True,
             digits_compute=dp.get_precision('Product UoS')),
         'quantity_dummy': fields.related(
@@ -138,7 +138,6 @@ class account_invoice_line(osv.osv):
         #   a defined product.uos and product.uos_coeff
         # This allows to modify one of this values and autocalculate the rest.
         # This takes care of units within the same category and their factor.
-        # 
     }
 
     _defaults = {
@@ -151,7 +150,7 @@ class account_invoice_line(osv.osv):
         'product_has_uos': _has_uos,
         'quantity_uom': _get_uos_id,
         'price_unit_uom': _price_unit_default,
-        'uos_id' : _get_uos_id,
+        'uos_id': _get_uos_id,
     }
 
     def product_id_change(self, cr, uid, ids, product, uom, qty=0, name='',
@@ -177,25 +176,26 @@ class account_invoice_line(osv.osv):
         context = dict(context)
         context.update({'company_id': company_id})
         # Call super() for compatibility
-        res = super(account_invoice_line, self).product_id_change(cr, uid, ids,
-            product, uom, qty, name, type, partner_id, fposition_id,
-            price_unit, address_invoice_id, currency_id, context=context)
+        res = super(account_invoice_line, self).product_id_change(
+            cr, uid, ids, product, uom, qty, name, type, partner_id,
+            fposition_id, price_unit, address_invoice_id, currency_id,
+            context=context)
             # , company_id)
 
         # Will reset quantity_uom to 1.0 as a initial value
-        res['value'].update({'quantity_uom': 1.0,})
+        res['value'].update({'quantity_uom': 1.0, })
         if not product:
             res['domain'].update({'uom_id': [('category_id', '!=', False)],
                                   'uos_id': [('category_id', '!=', False)]
                                   })
             res['value'].update({'product_has_uos': False,
-#                                  'cancel_uos': True,
-#                                  'cancel_uos_qty': True,
-#                                  'cancel_uos_price': True,
+                                 # 'cancel_uos': True,
+                                 # 'cancel_uos_qty': True,
+                                 # 'cancel_uos_price': True,
                                  'uos_id': False,
-#                                 'uos_id_dummy': False,  # Redundant
+                                 # 'uos_id_dummy': False,  # Redundant
                                  'uom_id': False,
-#                                 'quantity_uom': 1.0,
+                                 # 'quantity_uom': 1.0,
                                  'price_unit_uom': 0.0,
                                  })
             return res
@@ -203,7 +203,7 @@ class account_invoice_line(osv.osv):
         # If there is a product defined:
         product_obj = self.pool.get('product.product')
         prod = product_obj.browse(cr, uid, product, context=context)
-        
+
         new_uom_id = prod.uom_id and prod.uom_id.id or False
         new_uos_id = prod.uos_id and prod.uos_id.id or new_uom_id
         has_uos = prod.uos_id and True or False
@@ -222,9 +222,9 @@ class account_invoice_line(osv.osv):
                       '\t prod.uom_id.id : {}\n').format(prod.uom_id.id))
 
         res['value'].update({'product_has_uos': has_uos,
-#                                  'cancel_uos': not has_uos,
-#                                  'cancel_uos_qty': not has_uos,
-#                                  'cancel_uos_price': not has_uos,
+                             # 'cancel_uos': not has_uos,
+                             # 'cancel_uos_qty': not has_uos,
+                             # 'cancel_uos_price': not has_uos,
                              })
         product_uom_obj = self.pool.get('product.uom')
         new_qty_uom = 1.0
@@ -242,21 +242,21 @@ class account_invoice_line(osv.osv):
             currency_id, context=context)
 
         new_price_unit_uos = product_uom_obj._price_swap_uom_uos(
-                cr, uid, product, 'uom2uos',
-                new_uom_id, new_price_unit_uom, new_uos_id)
-        
+            cr, uid, product, 'uom2uos', new_uom_id, new_price_unit_uom,
+            new_uos_id)
+
         logger.debug(('\n\n\n\n>>>> product_id_change -> '
                       '  new_price_unit_uos: {}\n'
                       '  old price_unit    : {}\n'
                       '  new_price_unit_uom: {}\n'
                       '  ').format(new_price_unit_uom, price_unit,
                                    new_price_unit_uom))
-#             result['price_unit'] = price_unit_uom  # might be False
+        # result['price_unit'] = price_unit_uom  # might be False
         warning = {}
         warning.update(pu_uom_warning)
         res['value'].update({'uom_id': new_uom_id,
                              'uos_id': new_uos_id,
-#                              'uos_id_dummy': new_uos_id.id,
+                             # 'uos_id_dummy': new_uos_id.id,
                              'quantity_uom': new_qty_uom,
                              'quantity': new_qty_uos,
                              'price_unit_uom': new_price_unit_uom,
@@ -269,32 +269,32 @@ class account_invoice_line(osv.osv):
         # NOTE: Cannot force it to True because if value doesn't change
         #       the flag will affect next change, uncontrollably
         res['value'].update({
-             'cancel_uom': (new_uom_id != uom_id),
-             'cancel_uom_qty': (new_qty_uom != qty_uom),
-             'cancel_uom_price': (new_price_unit_uom != price_unit_uom),
-             'cancel_uos': (new_uos_id != uos_id),
-             'cancel_uos_qty': (new_qty_uos != qty_uos),
-             'cancel_uos_price': (new_price_unit_uos != price_unit_uos),
-             })
+            'cancel_uom': (new_uom_id != uom_id),
+            'cancel_uom_qty': (new_qty_uom != qty_uom),
+            'cancel_uom_price': (new_price_unit_uom != price_unit_uom),
+            'cancel_uos': (new_uos_id != uos_id),
+            'cancel_uos_qty': (new_qty_uos != qty_uos),
+            'cancel_uos_price': (new_price_unit_uos != price_unit_uos),
+        })
 
         # Restrain the UoM drop menu to the its category
         prod_uom_cat = prod.uom_id and prod.uom_id.category_id.id or False
         if prod_uom_cat:
             res['domain'].update({
-                'uom_id': [('category_id','=',prod_uom_cat)]
+                'uom_id': [('category_id', '=', prod_uom_cat)]
             })
         else:
-            res['domain'].update({'uom_id': [('category_id','!=',False)]})
+            res['domain'].update({'uom_id': [('category_id', '!=', False)]})
         # Restrain the UoS drop menu to the its category
         prod_uos_cat = prod.uos_id and prod.uos_id.category_id.id or \
             prod_uom_cat
 
         if prod_uos_cat:
             res['domain'].update({
-                'uos_id': [('category_id','=',prod_uos_cat)]
+                'uos_id': [('category_id', '=', prod_uos_cat)]
             })
         else:
-            res['domain'].update({'uos_id': [('category_id','!=',False)]})
+            res['domain'].update({'uos_id': [('category_id', '!=', False)]})
         return res
 
     # ---- UoM
@@ -338,8 +338,8 @@ class account_invoice_line(osv.osv):
         if product_id:
             product_obj = self.pool.get('product.product')
             prod = product_obj.browse(cr, uid, product_id, context=context)
-            
-            product_uom_obj = self.pool.get('product.uom')            
+
+            product_uom_obj = self.pool.get('product.uom')
             qty_uom_real = product_uom_obj._compute_qty(cr, uid,
                                                         uom_id, qty_uom,
                                                         prod.uom_id.id)
@@ -356,7 +356,7 @@ class account_invoice_line(osv.osv):
             warning.update(pu_uom_warning)
         else:
             new_price_unit_uom = price_unit_uom
-        
+
         # Price (UoM)
         diff1 = (new_price_unit_uom != price_unit_uom)
         if diff1:
@@ -372,7 +372,7 @@ class account_invoice_line(osv.osv):
         # Cast needed. See NOTE: [1]
         qty_uom = float(qty_uom)
         qty_uos = float(qty_uos)
-        
+
         if not uos_id:
             new_qty_uos = qty_uom
             new_price_unit_uos = new_price_unit_uom
@@ -380,7 +380,7 @@ class account_invoice_line(osv.osv):
             # If not product defined, it is user's responsability
             # If there is no product defined, or it has no defined UoS,
             # but uos_id field exists, we just compute the new quantity
-            # May be the case of a uom_id != uos_id, but in the same category 
+            # May be the case of a uom_id != uos_id, but in the same category
             product_uom_obj = self.pool.get('product.uom')
             new_qty_uos = product_uom_obj._compute_qty(cr, uid,
                                                        uom_id, qty_uom, uos_id)
@@ -442,13 +442,13 @@ class account_invoice_line(osv.osv):
         # If not product defined, it is user's responsibility
         if not product_id or not product_has_uos:
             # We will update only uos_id, without further triggers
-#             context.update({'skip_uos': True})
+            # context.update({'skip_uos': True})
             value.update({
                 'cancel_uos_qty': True,
                 'cancel_uos_price': True,
                 'cancel_uos': True,
                 'uos_id': uom_id,
-#                 'uos_id_dummy': uom_id
+                # 'uos_id_dummy': uom_id
             })
             return {'value': value, 'context': context}
 
@@ -463,9 +463,9 @@ class account_invoice_line(osv.osv):
                             'Calculations based on it were skipped.'),
             }
             return {'value': value, 'context': context, 'warning': warning}
-        
+
         # So far, product.product has a UoS defined, but in this form
-        # uos_id may or maybe not be set. 
+        # uos_id may or maybe not be set.
         if not uos_id:
             # We just warn, it is user's responsibility to choose a UoS
             warning = {
@@ -474,15 +474,15 @@ class account_invoice_line(osv.osv):
                             'Calculations based on it were skipped.'),
             }
             return {'value': value, 'context': context, 'warning': warning}
-        
+
         # Finally, if all is set correctly (product_id, product_has_uos,
         # uom_id, uos_id), we calculate accordingly quantity(UoS) and
-        # price_unit(UoS) 
-        
+        # price_unit(UoS)
+
         # NOTE: [1]
         # ---------
         # Is this cast needed?
-        # Yes, we need to cast in float because the value received from 
+        # Yes, we need to cast in float because the value received from
         # web client maybe an integer (Javascript and JSON do not
         # make any difference between 3.0 and 3). This cause a problem if
         # you encode, for example, 2 liters at 1.5 per
@@ -570,10 +570,10 @@ class account_invoice_line(osv.osv):
         elif not product_id or not product_has_uos:
             # If there is no product defined, or it has no defined UoS,
             # but uos_id field exists, we just compute the new price
-            # Maybe  the case of a uom_id != uos_id, but in the same category 
+            # Maybe  the case of a uom_id != uos_id, but in the same category
             product_uom_obj = self.pool.get('product.uom')
-            new_price_unit_uos = product_uom_obj._compute_price(cr, uid,
-                                                uom_id, price_unit_uom, uos_id)
+            new_price_unit_uos = product_uom_obj._compute_price(
+                cr, uid, uom_id, price_unit_uom, uos_id)
         else:
             product_uom_obj = self.pool.get('product.uom')
             new_price_unit_uos = product_uom_obj._price_swap_uom_uos(
@@ -725,7 +725,8 @@ class account_invoice_line(osv.osv):
                 'cancel_uom': True,
                 'uom_id': uos_id,
             })
-            logger.debug('\n\n >>>> onchange_uos_id -> value(3): {}\n\n'.format(value))
+            logger.debug('\n\n >>>> onchange_uos_id -> value(3): {}'
+                         '\n\n'.format(value))
             return {'value': value, 'context': context}
         else:
             value.update({
@@ -737,13 +738,13 @@ class account_invoice_line(osv.osv):
         #       depending on whether comes from a sales order or not (insane!)
         # ----------------------------------
         # So far, product.product has a UoS defined, but in this form
-        # uos_id may or maybe not be set. 
+        # uos_id may or maybe not be set.
         if not uos_id:
             # We just warn, it is user's responsibility to choose a UoS
             warning = {
                 'title': _('Unit of sale not set'),
                 'message': _('Unit of sale (UoS) must be set.\n'
-                            'Calculations based on it were skipped.'),
+                             'Calculations based on it were skipped.'),
             }
             return {'value': value, 'context': context, 'warning': warning}
 
@@ -752,13 +753,13 @@ class account_invoice_line(osv.osv):
             warning = {
                 'title': _('Unit of measure not set'),
                 'message': _('Unit of measure (UoM) must be set.\n'
-                            'Calculations based on it were skipped.'),
+                             'Calculations based on it were skipped.'),
             }
             return {'value': value, 'context': context, 'warning': warning}
 
         # Finally, if all is set correctly (product_id, product_has_uos,
         # uom_id, uos_id), we calculate accordingly quantity(UoS) and
-        # price_unit(UoS) 
+        # price_unit(UoS)
 
         # Calculate new quantity_uom and price_unit_uom (UoM) accordingly to
         # product.uos_coeff, AND ALSO that the selected UoM or UoS in this
@@ -829,7 +830,6 @@ class account_invoice_line(osv.osv):
             logger.debug('>>>> onchange_price_unit -> Cancelled!')
             return {'value': value, 'context': context}
         # ---------------------------------------------
-        
 
         logger.debug('>>>> onchange_price_unit ---> Passed skip_uos_price')
         product_uom_obj = self.pool.get('product.uom')
@@ -846,3 +846,93 @@ class account_invoice_line(osv.osv):
         return {'value': value, 'context': context}
 
 account_invoice_line()
+
+
+class account_invoice(osv.osv):
+    _inherit = "account.invoice"
+
+    def refund(self, cr, uid, ids, date=None, period_id=None, description=None,
+               journal_id=None):
+
+        invoices = self.read(cr, uid, ids,
+                             ['name', 'type', 'number', 'reference', 'comment',
+                              'date_due', 'partner_id', 'address_contact_id',
+                              'address_invoice_id', 'partner_contact',
+                              'partner_insite', 'partner_ref', 'payment_term',
+                              'account_id', 'currency_id', 'invoice_line',
+                              'tax_line', 'journal_id', 'user_id',
+                              'fiscal_position'])
+
+        obj_invoice_line = self.pool.get('account.invoice.line')
+        obj_invoice_tax = self.pool.get('account.invoice.tax')
+        obj_journal = self.pool.get('account.journal')
+        new_ids = []
+        for invoice in invoices:
+            del invoice['id']
+
+            type_dict = {
+                'out_invoice': 'out_refund',  # Customer Invoice
+                'in_invoice': 'in_refund',  # Supplier Invoice
+                'out_refund': 'out_invoice',  # Customer Refund
+                'in_refund': 'in_invoice',  # Supplier Refund
+            }
+
+            invoice_lines = obj_invoice_line.read(cr, uid,
+                                                  invoice['invoice_line'])
+            invoice_lines = self._refund_cleanup_lines(cr, uid, invoice_lines)
+
+            tax_lines = obj_invoice_tax.read(cr, uid, invoice['tax_line'])
+            tax_lines = filter(lambda l: l['manual'], tax_lines)
+            tax_lines = self._refund_cleanup_lines(cr, uid, tax_lines)
+            if journal_id:
+                refund_journal_ids = [journal_id]
+            elif invoice['type'] == 'in_invoice':
+                refund_journal_ids = obj_journal.search(
+                    cr, uid, [('type', '=', 'purchase_refund')])
+            else:
+                refund_journal_ids = obj_journal.search(
+                    cr, uid, [('type', '=', 'sale_refund')])
+
+            if not date:
+                date = time.strftime('%Y-%m-%d')
+            invoice.update({
+                'type': type_dict[invoice['type']],
+                'date_invoice': date,
+                'state': 'draft',
+                'number': False,
+                'invoice_line': invoice_lines,
+                'tax_line': tax_lines,
+                'journal_id': refund_journal_ids
+            })
+
+            if period_id:
+                invoice.update({
+                    'period_id': period_id,
+                })
+
+            if description:
+                invoice.update({
+                    'name': description,
+                })
+
+            # take the id part of the tuple returned for many2one fields
+            for field in ('address_contact_id', 'address_invoice_id',
+                          'partner_id', 'account_id', 'currency_id',
+                          'payment_term', 'journal_id', 'user_id',
+                          'fiscal_position'):
+                invoice[field] = invoice[field] and invoice[field][0]
+
+            for inv_line_tuple in invoice.get('invoice_line', []):
+                for tuple_pos in inv_line_tuple:
+                    if isinstance(tuple_pos, dict) and tuple_pos.get('uom_id',
+                                                                     False):
+                        tuple_pos.update({
+                            'uom_id': (tuple_pos['uom_id'] and
+                                       tuple_pos['uom_id'][0]),
+                        })
+            # create the new invoice
+            new_ids.append(self.create(cr, uid, invoice))
+
+        return new_ids
+
+account_invoice()
